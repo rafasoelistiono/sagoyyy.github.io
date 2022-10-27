43824826238be5c825f731c82b8d6634d5e8a45c
rafasoelistiono <71561783+rafasoelistiono@users.noreply.github.com>
2022-10-27 21:49:22 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+window.onload=function(){
+    setTimeout(content, 500);
+}
+
+function content(){
+    var he=document.querySelector('#home h2');
+    he.style.opacity='1';
+        he.style.transform='translateY(0)';
+    var hs=document.querySelector('#home p');
+    hs.style.opacity='1';
+        hs.style.transform='translateY(0)';
+    var hh=document.querySelector('.box-button');
+    hh.style.opacity='1';
+        hh.style.transform='translateY(0)';
+}
